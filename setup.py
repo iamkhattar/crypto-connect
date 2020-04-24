464c246c37e6e9fde6abea0ea87bb32df0053fbc
iamkhattar <shivamkhattar1@gmail.com>
2020-04-24 19:50:14 +0100

    CLI Finally Working

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
+from setuptools import find_packages
+
 setup(
     name='crypto-connect',
     version='1.0.0',
-    packages=['crypto-connect'],
+    packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'crypto-connect = crypto-connect.__main__:main'
+            'crypto-connect = crypto_connect.__main__:main'
         ]
     })
