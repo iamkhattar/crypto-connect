6049f96e58b07d63fb13035aaf7c17db63144a4d
iamkhattar <shivamkhattar1@gmail.com>
2020-04-25 03:16:04 +0100

    Prepping for v1.0.2

--- setup.py
+++ setup.py
@@ -10,15 +10,15 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='crypto-connect',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     license='MIT',
     description='CLI tool to track cryptocurrencies',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author='Shivam Khattar <shivamkhattar1@gmail.com > , Khaled Alqallaf <kalqallaf1@outlook.com> , Saeed Khan < saeed786129@hotmail.com >',
+    author='Shivam Khattar <shivamkhattar1@gmail.com> , Khaled Alqallaf <kalqallaf1@outlook.com> , Saeed Khan <saeed786129@hotmail.com >',
     url='https://iamkhattar.github.io/crypto-connect/#/',
-    download_url='https://github.com/iamkhattar/crypto-connect/archive/1.0.1.tar.gz',
+    download_url='',
     keywords=['CLI', 'cryptocurrency', 'crypto currency', 'market trends'],
     install_requires=['pycoingecko', 'Click', 'tabulate'],
     entry_points={
